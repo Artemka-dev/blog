1fc6d08853a0b180ca56bfd02894645e4901a580
Artemka-dev <artemka_pro@inbox.ru>
2020-01-01 12:03:14 +0300

    Migrations

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+
+	title = models.CharField(max_length=100, db_index=True)
+	body = models.TextField(db_index=True, blank=True)
+	date_pub = models.DateField(auto_now_add=True, blank=True, null=True)
+
+	def __str__(self):
+		return self.title
