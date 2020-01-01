3fb54030411fefab93a292b064fc471795bfb1d3
Artemka-dev <artemka_pro@inbox.ru>
2020-01-01 11:53:05 +0300

    Django routing

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,6 @@
 from django.shortcuts import render
-from django.views.generic import View
-from django.http import HttpResponse
+from django.views.generic import View, TemplateView
 
 # Create your views here.
-class HomePage(View):
-	
-	def get(self, request):
-		return render(request, "blog/home.html")
+class HomePage(TemplateView):
+	template_name = "blog/home.html"
